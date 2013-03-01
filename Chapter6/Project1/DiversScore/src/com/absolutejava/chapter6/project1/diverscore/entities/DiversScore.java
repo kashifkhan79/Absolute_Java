85020cf62232909ccbace8daa95cd79e5d0fcd11
Kashif Ahmed Khan <kashif_fast98@hotmail.com>
2013-03-01 20:50:21 +0100

    AbsoluteJava Chapter6 Project1 Divers Score TDD
    Updated getScores() as well as renamed NUMBER_OF_JUDGES to INDEX_MAX.
    Extracted multiple methods to validate scoresLength, individualScore,
    diveDifficulty

--- Chapter6/Project1/DiversScore/src/com/absolutejava/chapter6/project1/diverscore/entities/DiversScore.java
+++ Chapter6/Project1/DiversScore/src/com/absolutejava/chapter6/project1/diverscore/entities/DiversScore.java
@@ -11,10 +11,10 @@ import java.util.Arrays;
  * @author Kashif Ahmed Khan
  */
 public class DiversScore {
-    private static final int NUMBER_OF_JUDGES = 7;
-    private static final int INDIVIDUAL_SCORE_MIN = 1;
-    private static final int INDIVIDUAL_SCORE_MAX = 10;
     private static final int INDEX_MIN = 0;
+    private static final int INDEX_MAX = 7;
+    private static final int INDIVIDUAL_SCORE_MIN = 1;
+    private static final int INDIVIDUAL_SCORE_MAX = 10;    
     private static final float DIVER_SCORE_CONSTANT = 0.6F;
     private static final float DIVE_DIFFICULTY_MIN = 1.2F;
     private static final float DIVE_DIFFICULTY_MAX = 3.8F;
@@ -22,32 +22,32 @@ public class DiversScore {
     private float diveDifficulty;
     
     public DiversScore() {
-        this.scores = new float[NUMBER_OF_JUDGES];
+        this.scores = new float[INDEX_MAX];
     }
 
-    void setScores(float[] scores) throws ArrayIndexOutOfBoundsException {
-        if(scores.length > NUMBER_OF_JUDGES) {
+    public void setScores(float[] scores) throws ArrayIndexOutOfBoundsException {
+        if(isScoreLengthInvalid(scores)) {
             throw new ArrayIndexOutOfBoundsException("A Dive can only be scored by 7 judges");
         }
         this.scores = scores;
     }
     
     public float[] getScores() {
-        return scores;
+        return Arrays.copyOfRange(scores, INDEX_MIN, INDEX_MAX);
     }
 
     public void setIndividualScore(int index, float individualScore) throws ArrayIndexOutOfBoundsException, IllegalArgumentException {
-        if(index < INDEX_MIN || index > NUMBER_OF_JUDGES) {
+        if(isIndexInvalid(index)) {
             throw new ArrayIndexOutOfBoundsException("Index should be between 0 and 7");
         }
-        if(individualScore < INDIVIDUAL_SCORE_MIN || individualScore > INDIVIDUAL_SCORE_MAX) {
+        if(isIndividualScoreInvalid(individualScore)) {
             throw new IllegalArgumentException("Score should be between 0 and 10");
         }        
         this.scores[index] = individualScore;
     }
 
     public void setDiveDifficulty(float diveDifficulty) {
-        if(diveDifficulty < DIVE_DIFFICULTY_MIN || diveDifficulty > DIVE_DIFFICULTY_MAX) {
+        if(isDiveDifficultyInvalid(diveDifficulty)) {
             throw new IllegalArgumentException("Difficulty should be between 1.2 and 3.8");
         }
         this.diveDifficulty = diveDifficulty;
@@ -81,4 +81,21 @@ public class DiversScore {
     private float getHighestScore() {
         return scores[scores.length-1];
     }     
+
+    private boolean isScoreLengthInvalid(float[] scores) {
+        return (scores.length > INDEX_MAX);
+    }
+    
+    private boolean isIndexInvalid(int index) {
+        return (index < INDEX_MIN || index > INDEX_MAX);
+    }
+
+    private boolean isIndividualScoreInvalid(float individualScore) {
+        return (individualScore < INDIVIDUAL_SCORE_MIN || individualScore > INDIVIDUAL_SCORE_MAX);
+    }
+
+    private boolean isDiveDifficultyInvalid(float diveDifficulty) {
+        return (diveDifficulty < DIVE_DIFFICULTY_MIN || diveDifficulty > DIVE_DIFFICULTY_MAX);
+    }
+    
 }
